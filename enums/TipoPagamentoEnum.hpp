0ef3e59322e09de8f983bddb8776b7a25a745812
mario pedrao <mariomapedrao@gmail.com-at-851479049298>
2025-05-26 20:22:16 -0300

    incluindo atributos e declaracao da classe pagamento - correcao no enum

--- enums/TipoPagamentoEnum.hpp
+++ enums/TipoPagamentoEnum.hpp
@@ -4,6 +4,16 @@
 
 #pragma once
 
-enum TipoPagamentoEnum {
-    PIX = "P", DINHEIRO_FISICO = "F", CARTAO_DEBITO = "D", CARTAO_CREDITO = "C"
+enum class TipoPagamentoEnum {
+    PIX, DINHEIRO_FISICO, CARTAO_DEBITO, CARTAO_CREDITO
 };
+
+inline char toChar(TipoPagamentoEnum tipo) {
+    switch (tipo) {
+        case TipoPagamentoEnum::PIX: return 'P';
+        case TipoPagamentoEnum::DINHEIRO_FISICO: return 'F';
+        case TipoPagamentoEnum::CARTAO_DEBITO: return 'D';
+        case TipoPagamentoEnum::CARTAO_CREDITO: return 'C';
+        default: return 'z';
+    }
+}
