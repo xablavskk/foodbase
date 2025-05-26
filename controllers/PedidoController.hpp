fb7a27ae39c8b66027169084d58caaa63c25cf64
mario pedrao <mariomapedrao@gmail.com-at-851479049298>
2025-05-26 19:36:21 -0300

    AJUSTES NA REGRA DE NEGOCIO DA APLICACAO

--- controllers/PedidoController.hpp
+++ controllers/PedidoController.hpp
@@ -1,12 +1,16 @@
 #pragma once
+
 #include "../models/Pedido.hpp"
+#include "../services/PedidoService.hpp"
 #include "../db/Database.hpp"
 
 class PedidoController {
 private:
     Database db;
+    PedidoService service;
 
 public:
-    Pedido salvarPedido(const Pedido &pedido) const;
+    PedidoController();
+    Pedido salvarPedido(const Pedido& pedido) const;
     Pedido buscarPedidoPorId(int id) const;
 };
---
+++ services/PedidoService.cpp
@@ -0,0 +1,50 @@
+#include "PedidoService.hpp"
+
+#include <iostream>
+#pragma once
+
+PedidoService::PedidoService(Database& dbRef) : db(dbRef) {}
+
+Pedido PedidoService::salvarPedido(const Pedido& pedido) const {
+    if (pedido.getName().empty()) {
+        throw std::invalid_argument("Nome do pedido não pode ser vazio.");
+    }
+
+    std::stringstream ss;
+    ss << "INSERT INTO pedidos (id, nome) VALUES ("<< pedido.getId() << ", '" << pedido.getName() << "') RETURNING id;";
+
+    auto result = db.executarQuery(ss.str());
+
+    if (result.empty()) {
+        throw std::runtime_error("Erro ao salvar pedido: resultado vazio.");
+    }
+
+    return pedido;
+}
+
+Pedido PedidoService::buscarPedidoPorId(int id) const {
+    if (id == 0) {
+        throw std::invalid_argument("Id inválido.");
+    }
+
+    std::stringstream ss;
+    ss << "SELECT * FROM pedidos WHERE id = " << id << ";";
+
+    auto result = db.executarQuery(ss.str());
+
+    if (result.empty()) {
+        throw std::runtime_error("Erro ao buscar pedido: resultado vazio.");
+    }
+
+    int pedidoId = result[0]["id"].as<int>();
+    std::string nome = result[0]["nome"].as<std::string>();
+
+    std::cout<<"id"<<pedidoId;
+    std::cout<<"nome"<<nome;
+
+    Pedido pedido(pedidoId, nome);
+
+    return pedido;
+}
+
+
